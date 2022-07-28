d326035dc2acb884906d40e23b079adfb6fb287c
N3pomuceno <jnfazevedo@hotmail.com>
2022-07-28 00:08:20 -0300

    feat[button]: Cálculo por trás da média é feito

--- myactions.js
+++ myactions.js
@@ -1,9 +1,20 @@
 function adicao(){
     let dado = document.querySelector("input")
     contagem++;
-    soma += dado.value;
+    soma += parseFloat(dado.value);
+    // Falta adicionar o dado na parte central que mostra os valores adicionados
 }
 
+function calculoMedia() {
+    let med
+    if (contagem == 0) {
+        med = 0
+    } else {
+        med = soma/contagem
+    }
+    // usar innerHTML para colocar na pagina o resultado
+    console.log(contagem, soma, med)
+}
 
 
 let contagem = 0
@@ -14,3 +25,4 @@ btn1.addEventListener("click", adicao)
 
 
 var btn2 = document.querySelector("#button2")
+btn2.addEventListener("click", calculoMedia)
